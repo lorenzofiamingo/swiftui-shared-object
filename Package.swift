7b1d6e1b292ae4d7fab255cd81827be412a41b98
Lorenzo Fiamingo <l99fiamingo@gmail.com>
2022-02-25 21:10:34 +0100

    Add platforms

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-	name: "SwiftUI-SharedObject",
+    name: "SwiftUI-SharedObject",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),
         .tvOS(.v13),
         .watchOS(.v6)
     ],
-	products: [
-		.library(
-			name: "SharedObject",
-			targets: ["SharedObject"])
-	],
-	targets: [
-		.target(
-			name: "SharedObject"),
-		.testTarget(
-			name: "SharedObjectTests",
-			dependencies: ["SharedObject"])
+    products: [
+        .library(
+            name: "SharedObject",
+            targets: ["SharedObject"])
+    ],
+    targets: [
+        .target(
+            name: "SharedObject"),
+        .testTarget(
+            name: "SharedObjectTests",
+            dependencies: ["SharedObject"])
 	]
 )
